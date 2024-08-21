8eacd05d8870ff1341f9b5ffc13f346aceaa9ced
Val <valentin.seiler1@gmail.com>
2024-08-21 13:48:38 +0200

    update script realisation

--- scripts/realisations.js
+++ scripts/realisations.js
@@ -20,7 +20,7 @@ function updateUI() {
 
 // Affichage des stands 
 function showStands () {
-    for (i = 0; i < stands.length; i++) {
+    for (let i = 0; i < stands.length; i++) {
         // Création des éléments
         const aStandElement = document.createElement('a');
         const imgStandElement = document.createElement('img');
@@ -40,9 +40,59 @@ function showStands () {
         aStandElement.appendChild(pStandElement);
         pStandElement.appendChild(iStandElement);
         pStandElement.appendChild(textStandElement);
-        
+
+        // Action bouton gallerie
+        aStandElement.addEventListener('click', () => showFullscreenGallerie(i));
     }
 }
+
+// Gestion de l'affichage de la gallerie
+function showFullscreenGallerie(index) {
+  let indexPhoto = 0;
+
+  const divFullscreenElement = document.createElement('div');
+  divFullscreenElement.classList.add('fullscreen');
+
+  const imgFullscreenElement = document.createElement('img');
+  imgFullscreenElement.src = stands[index].photos.présentation;
+
+  const btnExitElement = document.createElement('button');
+  btnExitElement.innerText = 'X';
+
+  const btnPrevElement = document.createElement('button');
+  btnPrevElement.innerText = '<';
+
+  const btnNextElement = document.createElement('button');
+  btnNextElement.innerText = '>';
+
+  divFullscreenElement.appendChild(imgFullscreenElement);
+  divFullscreenElement.appendChild(btnExitElement);
+  divFullscreenElement.appendChild(btnPrevElement);
+  divFullscreenElement.appendChild(btnNextElement);
+
+  document.body.appendChild(divFullscreenElement);
+
+  btnExitElement.addEventListener('click', () => divFullscreenElement.remove());
+  //btnPrevElement.addEventListener('click', () => navigateImage(index, indexPhoto));
+  btnNextElement.addEventListener('click', () => navigateNextImage(index, indexPhoto));
+
+}
+
+// Navigation dans la gallerie
+
+function navigateNextImage(index, indexPhoto) {
+  // if (indexPhoto < 0) indexPhoto = stands[index].photos.autre.length;
+  // if (indexPhoto > stands[index].photos.autre.length) indexPhoto = 0;
+  
+  const fullscreenImage = document.querySelector('.fullscreen img');
+  fullscreenImage.src = stands[index].photos.autre[indexPhoto];
+  console.log(indexPhoto);
+  indexPhoto += 1
+  
+  console.log(indexPhoto);
+  return indexPhoto;
+}
+
 
 // Appel initializeStands au chargement de la page
 document.addEventListener('DOMContentLoaded', initializeStands);
